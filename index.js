83ce93c4ba00cac42afea6ec162cb664c0bb4675
Gene Connolly <geneconnolly@mw-gene.local>
2018-11-10 15:05:03 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const port = process.env.PORT || 3000
+
+app.get('/blue', function(req,res){
+  res.send('blue')
+});
+
+app.get('/green', function(req,res){
+    res.send('green')
+  });
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
